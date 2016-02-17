de2cad60033255e671e60c98f954b3d7300dbf97
dlellis <dlellis@unomaha.edu>
2016-02-17 13:20:16 -0800

    Steps 12 and 13

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -2,6 +2,8 @@ var EmployeeView = function(employee) {
 
   this.initialize = function() {
       this.$el = $('<div/>');
+      this.$el.on('click', '.add-location-btn', this.addLocation);
+      this.$el.on('click', '.add-contact-btn', this.addToContacts);
   };
 
   this.render = function() {
@@ -9,6 +11,40 @@ var EmployeeView = function(employee) {
       return this;
   };
 
+  this.addLocation = function(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(
+      function(position) {
+          alert(position.coords.latitude + ',' + position.coords.longitude);
+      },
+      function() {
+          alert('Error getting location');
+      });
+  return false;
+  };
+
+
+	this.addToContacts = function(event) {
+	    event.preventDefault();
+	    console.log('addToContacts');
+	    if (!navigator.contacts) {
+	        alert("Contacts API not supported", "Error");
+	        return;
+	    }
+	    var contact = navigator.contacts.create();
+	    contact.name = {givenName: employee.firstName, familyName: employee.lastName};
+	    var phoneNumbers = [];
+	    phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
+	    phoneNumbers[1] = new ContactField('mobile', employee.cellPhone, true);
+	    contact.phoneNumbers = phoneNumbers;
+	    contact.save();
+	    return false;
+	};
+  
   this.initialize();
 
+
+
+
 }
+
